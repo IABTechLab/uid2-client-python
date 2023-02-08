e02e11e41d5b84202fc76081f686554fe69bc405
Sunny Wu <sunny.wu@thetradedesk.com>
2023-02-08 15:57:54 +1100

    Changed ADVERTISING_TOKEN_V4 enum to 128 for readability in Base64

--- uid2_client/encryption.py
+++ uid2_client/encryption.py
@@ -25,8 +25,11 @@ class _PayloadType(Enum):
     ENCRYPTED_DATA_V3 = 96
 
 class _AdvertisingTokenCode(Enum):
+    # showing as "AHA..." in the Base64 Encoding (Base64 'H' is 000111 and 112 is 01110000)
     ADVERTISING_TOKEN_V3 = 112
-    ADVERTISING_TOKEN_V4 = 118
+    # showing as "AIA..." in the Base64URL Encoding ('H' is followed by 'I' hence
+    # this choice for the next token version) (Base64 'I' is 001000 and 128 is 10000000)
+    ADVERTISING_TOKEN_V4 = 128
 
 base64_url_special_chars = {"-", "_"}
 
