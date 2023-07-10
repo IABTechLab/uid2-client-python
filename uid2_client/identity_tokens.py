fdc7890c9258fcfe0490f05c1204fc7657a8bf63
ian.nara <ian.nara@thetradedesk.com>
2023-07-10 16:51:14 -0600

    make identity tokens attributes internal

--- uid2_client/identity_tokens.py
+++ uid2_client/identity_tokens.py
@@ -5,13 +5,13 @@ import json
 class IdentityTokens:
     def __init__(self, advertising_token, refresh_token, refresh_response_key, identity_expires,
                  refresh_expires, refresh_from, json_string):
-        self.advertising_token = advertising_token
-        self.refresh_token = refresh_token
-        self.refresh_response_key = refresh_response_key
-        self.identity_expires = identity_expires
-        self.refresh_expires = refresh_expires
-        self.refresh_from = refresh_from
-        self.json_string = json_string
+        self._advertising_token = advertising_token
+        self._refresh_token = refresh_token
+        self._refresh_response_key = refresh_response_key
+        self._identity_expires = identity_expires
+        self._refresh_expires = refresh_expires
+        self._refresh_from = refresh_from
+        self._json_string = json_string
 
     @staticmethod
     def from_json_string(json_string):
@@ -42,37 +42,37 @@ class IdentityTokens:
         return self.is_due_for_refresh_impl(datetime.datetime.now())
 
     def get_advertising_token(self):
-        return self.advertising_token
+        return self._advertising_token
 
     def get_refresh_token(self):
-        return self.refresh_token
+        return self._refresh_token
 
     def get_json_string(self):
-        return self.json_string
+        return self._json_string
 
     def is_refreshable(self):
         return self.is_refreshable_impl(datetime.datetime.now())
 
     def is_refreshable_impl(self, timestamp):
-        refresh_expires = self.refresh_expires
+        refresh_expires = self._refresh_expires
         if refresh_expires is None or timestamp.timestamp() > refresh_expires:
             return False
-        return self.refresh_token is not None
+        return self._refresh_token is not None
 
     def is_due_for_refresh_impl(self, timestamp):
-        return timestamp.timestamp() > self.refresh_from or self.has_identity_expired(timestamp)
+        return timestamp.timestamp() > self._refresh_from or self.has_identity_expired(timestamp)
 
     def has_identity_expired(self, timestamp):
-        return timestamp.timestamp() > self.identity_expires
+        return timestamp.timestamp() > self._identity_expires
 
     def get_refresh_response_key(self):
-        return self.refresh_response_key
+        return self._refresh_response_key
 
     def get_identity_expires(self):
-        return self.identity_expires
+        return self._identity_expires
 
     def get_refresh_expires(self):
-        return self.refresh_expires
+        return self._refresh_expires
 
     def get_refresh_from(self):
-        return self.refresh_from
+        return self._refresh_from
