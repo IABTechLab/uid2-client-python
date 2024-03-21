b4d70acd3ee33bcd1cf7e81812cd6e563974fbbc
asloob qureshi <asloob.qureshi@thetradedesk.com>
2024-03-21 11:09:43 -0700

    Fix existing tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,6 +8,7 @@ from uid2_client.euid_client_factory import EuidClientFactory
 from uid2_client.uid2_client_factory import Uid2ClientFactory
 
 
+@patch('uid2_client.client.refresh_sharing_keys')
 class TestClient(unittest.TestCase):
     class MockPostResponse:
         def __init__(self, return_value):
@@ -31,7 +32,6 @@ class TestClient(unittest.TestCase):
     def _get_post_refresh_keys_response(self, base_url, path, headers, data):
         response_payload = key_set_to_json_for_sharing([master_key, site_key]).encode()
         return self._make_post_response(data, response_payload)
-
 
     def _validate_master_and_site_key(self, keys):
         self.assertEqual(len(keys.values()), 2)
@@ -57,23 +57,25 @@ class TestClient(unittest.TestCase):
         self.assertEqual(master_secret, master.secret)
         self.assertEqual(1, master.keyset_id)
 
-    @patch('uid2_client.client.post')
-    def test_refresh(self, mock_post):
-        mock_post.side_effect = self._get_post_refresh_keys_response
+    def setUp(self):
+        key_set = [master_key, site_key]
+        self._key_collection = EncryptionKeysCollection(key_set, IdentityScope.UID2, site_id, 1,
+                                                        99999, 86400)
+
+    def test_refresh(self, mock_refresh_keys_util):
+        mock_refresh_keys_util.return_value = self._key_collection
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
-        keys = client._keys
-        self._validate_master_and_site_key(keys)
+        self._validate_master_and_site_key(client._keys)
 
-    def test_refresh_json(self):
+    def test_refresh_json(self, mock_refresh_keys_util):
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         keys_json = key_set_to_json_for_sharing([master_key, site_key])
         keys = client.refresh_json(keys_json)
         self._validate_master_and_site_key(keys)
 
-    @patch('uid2_client.client.post')
-    def test_encrypt_decrypt(self, mock_post):
-        mock_post.side_effect = self._get_post_refresh_keys_response
+    def test_encrypt_decrypt(self, mock_refresh_keys_util):
+        mock_refresh_keys_util.return_value = self._key_collection
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
@@ -84,29 +86,21 @@ class TestClient(unittest.TestCase):
         result = client.decrypt(ad_token)
         self.assertEqual(example_uid, result.uid2)
 
-    @patch('uid2_client.client.post')
-    def test_can_decrypt_another_clients_encrypted_token(self, mock_post):
-        mock_post.side_effect = self._get_post_refresh_keys_response
+    def test_can_decrypt_another_clients_encrypted_token(self, mock_refresh_keys_util):
+        mock_refresh_keys_util.return_value = self._key_collection
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
         ad_token = client.encrypt(example_uid)
 
-        def get_post_refresh_keys_response_with_header(base_url, path, headers, data):
-            response_payload = key_set_to_json_for_sharing_with_header('"default_keyset_id": 12345,', 4874,
-                                                                       [master_key, site_key]).encode()
-            return self._make_post_response(data, response_payload)
-
-        mock_post.side_effect = get_post_refresh_keys_response_with_header
         receiving_client = Uid2ClientFactory.create("endpoint2", "authkey2", client_secret)
         receiving_client.refresh_keys()
 
         result = receiving_client.decrypt(ad_token)
         self.assertEqual(example_uid, result.uid2)
 
-    @patch('uid2_client.client.post')
-    def test_sharing_token_is_v4(self, mock_post):
-        mock_post.side_effect = self._get_post_refresh_keys_response
+    def test_sharing_token_is_v4(self, mock_refresh_keys_util):
+        mock_refresh_keys_util.return_value = self._key_collection
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
@@ -114,27 +108,24 @@ class TestClient(unittest.TestCase):
         contains_base_64_special_chars = "+" in ad_token or "/" in ad_token or "=" in ad_token
         self.assertFalse(contains_base_64_special_chars)
 
-    @patch('uid2_client.client.post')
-    def test_uid2_client_produces_uid2_token(self, mock_post):
-        mock_post.side_effect = self._get_post_refresh_keys_response
+    def test_uid2_client_produces_uid2_token(self, mock_refresh_keys_util):
+        mock_refresh_keys_util.return_value = self._key_collection
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
         ad_token = client.encrypt(example_uid)
         self.assertEqual("A", ad_token[0])
 
-    @patch('uid2_client.client.post')
-    def test_euid_client_produces_euid_token(self, mock_post):
-        mock_post.side_effect = self._get_post_refresh_keys_response
+    def test_euid_client_produces_euid_token(self, mock_refresh_keys_util):
+        mock_refresh_keys_util.return_value = self._key_collection
         client = EuidClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
         ad_token = client.encrypt(example_uid)
         self.assertEqual("E", ad_token[0])
 
-    @patch('uid2_client.client.post')
-    def test_raw_uid_produces_correct_identity_type_in_token(self, mock_post):
-        mock_post.side_effect = self._get_post_refresh_keys_response
+    def test_raw_uid_produces_correct_identity_type_in_token(self, mock_refresh_keys_util):
+        mock_refresh_keys_util.return_value = self._key_collection
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
@@ -151,13 +142,11 @@ class TestClient(unittest.TestCase):
         self.assertEqual(IdentityType.Email,
                          get_token_identity_type("EKCoNGWPYng/whjBIwANJHKvvGlGyzARtDIAk95FlJyb", client._keys))
 
-    @patch('uid2_client.client.post')
-    def test_multiple_keys_per_keyset(self, mock_post):
-        def get_post_refresh_keys_response_with_multiple_keys(base_url, path, headers, data):
-            response_payload = key_set_to_json_for_sharing([master_key, site_key, master_key2, site_key2]).encode()
-            return self._make_post_response(data, response_payload)
+    def test_multiple_keys_per_keyset(self, mock_refresh_keys_util):
+        def get_post_refresh_keys_response_with_multiple_keys():
+            return create_default_key_collection([master_key, site_key, master_key2, site_key2])
 
-        mock_post.side_effect = get_post_refresh_keys_response_with_multiple_keys
+        mock_refresh_keys_util.return_value = get_post_refresh_keys_response_with_multiple_keys()
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
@@ -166,13 +155,11 @@ class TestClient(unittest.TestCase):
         result = client.decrypt(ad_token)
         self.assertEqual(example_uid, result.uid2)
 
-    @patch('uid2_client.client.post')
-    def test_cannot_encrypt_if_no_key_from_default_keyset(self, mock_post):
-        def get_post_refresh_keys_response_with_no_default_keyset_key(base_url, path, headers, data):
-            response_payload = key_set_to_json_for_sharing([master_key]).encode()
-            return self._make_post_response(data, response_payload)
+    def test_cannot_encrypt_if_no_key_from_default_keyset(self, mock_refresh_keys_util):
+        def get_post_refresh_keys_response_with_no_default_keyset_key():
+            return create_default_key_collection([master_key])
 
-        mock_post.side_effect = get_post_refresh_keys_response_with_no_default_keyset_key
+        mock_refresh_keys_util.return_value = get_post_refresh_keys_response_with_no_default_keyset_key()
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
@@ -180,13 +167,13 @@ class TestClient(unittest.TestCase):
             client.encrypt(example_uid)
             self.assertTrue('No Site ID in keys' in context.exception)
 
-    @patch('uid2_client.client.post')
-    def test_cannot_encrypt_if_theres_no_default_keyset_header(self, mock_post):
-        def get_post_refresh_keys_response_with_no_default_keyset_header(base_url, path, headers, data):
-            response_payload = key_set_to_json_for_sharing_with_header("", site_id, [master_key, site_key]).encode()
-            return self._make_post_response(data, response_payload)
+    def test_cannot_encrypt_if_theres_no_default_keyset_header(self, mock_refresh_keys_util):
+        def get_post_refresh_keys_response_with_no_default_keyset_header():
+            key_set = [master_key, site_key]
+            return EncryptionKeysCollection(key_set, IdentityScope.UID2, 9000, 1,
+                                            "", 86400)
 
-        mock_post.side_effect = get_post_refresh_keys_response_with_no_default_keyset_header
+        mock_refresh_keys_util.return_value = get_post_refresh_keys_response_with_no_default_keyset_header()
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
@@ -194,15 +181,11 @@ class TestClient(unittest.TestCase):
             client.encrypt(example_uid)
             self.assertTrue('No Keyset Key Found' in context.exception)
 
-    @patch('uid2_client.client.post')
-    def test_expiry_in_token_matches_expiry_in_response(self, mock_post):
-        def get_post_refresh_keys_response_with_token_expiry(base_url, path, headers, data):
-            response_payload = key_set_to_json_for_sharing_with_header('"default_keyset_id": 99999, '
-                                                                       '"token_expiry_seconds": 2,', 99999, [master_key,
-                                                                                                             site_key]).encode()
-            return self._make_post_response(data, response_payload)
+    def test_expiry_in_token_matches_expiry_in_response(self, mock_refresh_keys_util):
+        def get_post_refresh_keys_response_with_token_expiry():
+            return create_default_key_collection([master_key, site_key])
 
-        mock_post.side_effect = get_post_refresh_keys_response_with_token_expiry
+        mock_refresh_keys_util.return_value = get_post_refresh_keys_response_with_token_expiry()
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
@@ -223,18 +206,13 @@ class TestClient(unittest.TestCase):
                 client.decrypt(ad_token)
                 self.assertTrue('token expired' in context.exception)
 
+    def test_encrypt_key_inactive(self, mock_refresh_keys_util):
+        def get_post_refresh_keys_response_with_key_inactive():
+            inactive_key = EncryptionKey(245, site_id, now, now + dt.timedelta(days=1), now + dt.timedelta(days=2),
+                                         site_secret, keyset_id=99999)
+            return create_default_key_collection([inactive_key])
 
-
-
-    @patch('uid2_client.client.post')
-    def test_encrypt_key_inactive(self, mock_post):
-        def get_post_refresh_keys_response_with_key_inactive(base_url, path, headers, data):
-            key = EncryptionKey(245, site_id, now, now + dt.timedelta(days=1), now + dt.timedelta(days=2), site_secret,
-                                keyset_id=99999)
-            response_payload = key_set_to_json_for_sharing([master_key, key]).encode()
-            return self._make_post_response(data, response_payload)
-
-        mock_post.side_effect = get_post_refresh_keys_response_with_key_inactive
+        mock_refresh_keys_util.return_value = get_post_refresh_keys_response_with_key_inactive()
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
@@ -242,14 +220,13 @@ class TestClient(unittest.TestCase):
             client.encrypt(example_uid)
             self.assertTrue('No Keyset Key Found' in context.exception)
 
-    @patch('uid2_client.client.post')
-    def test_encrypt_key_expired(self, mock_post):
-        def get_post_refresh_keys_response_with_key_expired(base_url, path, headers, data):
-            key = EncryptionKey(245, site_id, now, now, now - dt.timedelta(days=1), site_secret, keyset_id=99999)
-            response_payload = key_set_to_json_for_sharing([master_key, key]).encode()
-            return self._make_post_response(data, response_payload)
+    def test_encrypt_key_expired(self, mock_refresh_keys_util):
+        def get_post_refresh_keys_response_with_key_expired():
+            expired_key = EncryptionKey(245, site_id, now, now, now - dt.timedelta(days=1), site_secret,
+                                        keyset_id=99999)
+            return create_default_key_collection([expired_key])
 
-        mock_post.side_effect = get_post_refresh_keys_response_with_key_expired
+        mock_refresh_keys_util.return_value = get_post_refresh_keys_response_with_key_expired()
         client = Uid2ClientFactory.create("base_url", "api_key", client_secret)
         client.refresh_keys()
 
