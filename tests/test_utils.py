e7334a319c501d603889b2c0de20ca179dc6b1c4
asloob qureshi <asloob.qureshi@thetradedesk.com>
2024-03-25 15:29:26 -0700

    More test cases for bidstream client

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,9 @@ import base64
 import datetime as dt
 from datetime import timezone
 
-from uid2_client import EncryptionKey, encrypt, IdentityScope, IdentityType, EncryptionKeysCollection
+from tests.uid2_token_generator import UID2TokenGenerator
+from uid2_client import EncryptionKey, encrypt, IdentityScope, IdentityType, EncryptionKeysCollection, \
+    AdvertisingTokenVersion
 
 master_secret = bytes(
     [139, 37, 241, 173, 18, 92, 36, 232, 165, 168, 23, 18, 38, 195, 123, 92, 160, 136, 185, 40, 91, 173, 165, 221, 168,
@@ -33,7 +35,28 @@ site_key2 = EncryptionKey(site_key_id, site_id, now - dt.timedelta(days=-2), now
 client_secret = "ioG3wKxAokmp+rERx6A4kM/13qhyolUXIu14WN16Spo="
 client_secret_bytes = base64.b64decode(client_secret)
 example_uid = "ywsvDNINiZOVSsfkHpLpSJzXzhr6Jx9Z/4Q0+lsEUvM="
+phone_uid = "BEOGxroPLdcY7LrSiwjY52+X05V0ryELpJmoWAyXiwbZ"
 
+test_cases_all_scopes_all_versions = [
+    [IdentityScope.UID2, AdvertisingTokenVersion.ADVERTISING_TOKEN_V2],
+    [IdentityScope.UID2, AdvertisingTokenVersion.ADVERTISING_TOKEN_V3],
+    [IdentityScope.UID2, AdvertisingTokenVersion.ADVERTISING_TOKEN_V4],
+    [IdentityScope.EUID, AdvertisingTokenVersion.ADVERTISING_TOKEN_V2],
+    [IdentityScope.EUID, AdvertisingTokenVersion.ADVERTISING_TOKEN_V3],
+    [IdentityScope.EUID, AdvertisingTokenVersion.ADVERTISING_TOKEN_V4]
+]
+
+test_cases_all_scopes_v3_v4_versions = [
+    [IdentityScope.UID2, AdvertisingTokenVersion.ADVERTISING_TOKEN_V3],
+    [IdentityScope.UID2, AdvertisingTokenVersion.ADVERTISING_TOKEN_V4],
+    [IdentityScope.EUID, AdvertisingTokenVersion.ADVERTISING_TOKEN_V3],
+    [IdentityScope.EUID, AdvertisingTokenVersion.ADVERTISING_TOKEN_V4]
+]
+
+YESTERDAY = now + dt.timedelta(days=-1)
+TOMORROW = now + dt.timedelta(days=1)
+IN_2_DAYS = now + dt.timedelta(days=2)
+IN_3_DAYS = now + dt.timedelta(days=3)
 
 def get_identity_type(token):
     first_char = token[0]
@@ -88,3 +111,14 @@ def format_key(key: EncryptionKey):
 def create_default_key_collection(key_set):
     return EncryptionKeysCollection(key_set, IdentityScope.UID2, 9000, 1,
                                     99999, 2)
+
+
+def create_key_collection(identity_scope):
+    key_set = [master_key, site_key]
+    return EncryptionKeysCollection(key_set, identity_scope, site_id, 1,
+                             99999, 86400)
+
+
+def generate_uid_token(identity_scope, version, raw_uid=example_uid, created_at=None, expires_at=None):
+    return UID2TokenGenerator.generate_uid_token(raw_uid, master_key, site_id, site_key,
+                                                 identity_scope, version, created_at, expires_at)
